2c8fdaba0c9a4039fa4bc71cf80598ba36393426
amcmanama <daimaoh@gmail.com>
2011-05-23 10:42:46 -0700

    adding javascript

---
+++ PraLoup.WebApp/Scripts/console.js
@@ -0,0 +1,25 @@
+﻿function loginp(uids, sesk) {
+    var str = FB.JSON.stringify({ uid: uids, sessionkey: sesk });
+   
+    $.post(
+        '/Praloup.WebApp/Account/Register',
+        { json: str },
+        function (data) {
+            
+        }
+    );
+}
+
+function EnableForm() {
+    var input = $("input").get();
+    for (var i = 0; i < input.length; i++) {
+        input[i].disabled = false;
+    }
+}
+
+function DisableForm() {
+    var input = $("input").get();
+    for (var i = 0; i < input.length; i++) {
+        input[i].disabled = true;
+    }
+}
